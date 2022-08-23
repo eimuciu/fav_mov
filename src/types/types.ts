1e215b4f82d2c4e1960ac60f7aca06d570ea7da4
ed <allgoodata@outlook.com>
2022-08-23 16:27:38 +0300

    mobile responsive

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,9 +5,3 @@ export interface Movie {
   Year: string;
   imdbID: string;
 }
-
-// Poster: "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
-// Title: "The Batman"
-// Type: "movie"
-// Year: "2022"
-// imdbID: "tt1877830"
